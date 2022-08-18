66e6e96ec66e35993fe8152a421c999c5d01253d
raevem <86608337+raevem@users.noreply.github.com>
2022-08-18 15:29:08 +0300

    change ami type instance

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
 
   tags = {
